5f8a7de69c465c4c55c2318027749501210a6c34
Ian Zhang <ianzhang1986@gmail.com>
2015-09-17 20:40:52 -0700

    Fix bloomFilter FNV hash functions

--- bloomFilter.js
+++ bloomFilter.js
@@ -8,14 +8,21 @@
 // FNV_PRIME = 2^10 + 2^8 + 
 var fnv18a = function fnv18a(str) {
   var b = 0xAD;
-  var FNV_PRIME = Math.pow(2, 10) + Math.pow(2, 8) + b;
-  var FNV1_18A_INIT = 0x811c9dc5;
-  var hval = FNV1_18A_INIT;
-  for ( var i = 0; i < str.length; ++i) {
-  	hval ^= str.charCodeAt(i); 		// unicode
-  	hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+  // var FNV_PRIME = Math.pow(2, 10) + Math.pow(2, 8) + b;
+  var FNV_PRIME = 0x5AD;
+  var offset_basis = 0;
+  // var FNV1_18A_INIT = 0x811c9dc5;
+  // for ( var i = 0; i < str.length; ++i) {
+  // 	hval ^= str.charCodeAt(i); 		// unicode
+  // 	hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+  // }
+  // return hval >>> 0;
+  var hash_bits = 18;
+  hash_mod = Math.pow(2, hash_bits);
+  for ( var i = 0, len = str.length; i < len; i++) {
+    offset_basis = (offset_basis * FNV_PRIME) % hash_mod;
+    // offset_basis ^=
   }
-  return hval >>> 0;
 }
 
 var bloomFilter = function bloomFilter() {
@@ -28,8 +35,6 @@ var bloomFilter = function bloomFilter() {
   // var bitMask = 0b00000000000000111111111111111111 
   var bitMask = 262143;
 
-  // bitMask & bitField with di
-  // bitMask & bitField // bitwise and?
 
 
 }
