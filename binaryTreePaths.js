6f351abc785ef68c5428211e5f977beb7e96ec07
Ian Zhang <ianzhang1986@gmail.com>
2015-11-12 12:24:58 -0800

    [Feature] Complete baseline solution for binaryTreePaht

--- binaryTreePaths.js
+++ binaryTreePaths.js
@@ -10,15 +10,15 @@
   *  @return {string[]}
   */
  var binaryTreePaths = function(root) {
-     // Travesal the tree using recursion while concat the strings
     var bucket = [];
+    if (root === null) return bucket;
     var recurse = function(node, str) {
-      if (node === null) {
-        bucket.push(str);
+      if (node.left === null && node.right === null) {
+        return bucket.push(str);
       }
-      if (node.left !== null) recurse(node.left, str + "");
-      if (node.right !== null) recurse(node.right, str + "");
+      if (node.left !== null) recurse(node.left, str + "->" + node.left.val);
+      if (node.right !== null) recurse(node.right, str + "->" + node.right.val);
     };
-    recurse(root);
+    recurse(root, "" + root.val);
     return bucket;
 };
