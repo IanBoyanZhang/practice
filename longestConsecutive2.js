8e41e328260972e3b060dad16c3c54dae0645161
Ian Zhang <ianzhang1986@gmail.com>
2015-11-14 01:44:08 -0800

    [Cleanup] Simplified writing in longesetConsecutiveString

--- longestConsecutive2.js
+++ longestConsecutive2.js
@@ -3,25 +3,29 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// Using iterative approach instead
 var longestConsecutive = function(nums) {
   var hashmap = {};
 
   if (nums.length === 0) return 0;
   if (nums.length === 1) return 1;
 
-  var traverseList = function(key, obj, counter) {
-    // if (obj.hasOwnProperty(key)) {
-    if (obj[key]) {
-      //return traverseList(obj[key], obj, counter+1);
-      return traverseList(+key+1, obj, counter+1);
-    }
-    return counter;
-  };
   // duplicated numbers? 
   // first pass
   for (var iter = 0, len = nums.length; iter < len; iter++) {
     hashmap[nums[iter]] = nums[iter] + 1;
   }
+
+  var traverseList = function(key, obj) {
+    var counter = 1, queue = []; queue.push(obj[key]);
+    var newKey;
+    while (queue.length) {
+      newKey = queue.shift();
+      if(obj.hasOwnProperty(obj[newKey])) { queue.push(obj[newKey]); counter+=1; }
+    }
+    return counter;
+  };
   // Second pass
   // declare necessary vars to record 
   var currLen, maxLen = 1, maxKey;
