3841903d4272c87423dcecea98f1965ead3ff468
Ian Zhang <ianzhang1986@gmail.com>
2015-11-14 12:05:19 -0800

    [Feature] Pass all test case using hash table solution

--- longestConsecutive2.js
+++ longestConsecutive2.js
@@ -6,36 +6,40 @@
 
 // Using iterative approach instead
 var longestConsecutive = function(nums) {
+  'use strict';
   var hashmap = {};
+  var hashdup = {};
 
   if (nums.length === 0) return 0;
   if (nums.length === 1) return 1;
 
   // duplicated numbers? 
   // first pass
+  var maxLen = 1; var prevKey;
   for (var iter = 0, len = nums.length; iter < len; iter++) {
-    hashmap[nums[iter]] = nums[iter] + 1;
+    if (hashdup[nums[iter]]) continue;
+
+    if (hashmap[nums[iter]] === undefined) { hashmap[nums[iter]] = 1; hashdup[nums[iter]] = true; }
+    if (hashmap[nums[iter]+1] !== undefined) { hashmap[nums[iter]] += hashmap[nums[iter]+1]; console.log("num " + nums[iter] + " " + hashmap[nums[iter]]);}
+
+    if (hashmap[nums[iter]] > maxLen) { maxLen = hashmap[nums[iter]]; }
+    // Keeps iterating till updating all previous ones ()
+    prevKey = nums[iter] - 1; 
+    while(hashmap[prevKey]) {
+      hashmap[prevKey] += hashmap[nums[iter]];
+      console.log("num " + nums[iter] + " " + hashmap[nums[iter]]);
+      if (hashmap[prevKey] > maxLen) { maxLen = hashmap[prevKey]; }
+      prevKey -= 1;
+    }
   }
 
-  var traverseList = function(key, obj) {
-    var counter = 1, queue = []; queue.push(obj[key]);
-    var newKey;
-    while (queue.length) {
-      newKey = queue.shift();
-      if(obj.hasOwnProperty(obj[newKey])) { queue.push(obj[newKey]); counter+=1; }
-    }
-    return counter;
-  };
-  // Second pass
-  // declare necessary vars to record 
-  var currLen, maxLen = 1, maxKey;
-  for (var key in hashmap) {
-    if (maxKey === undefined) maxKey = key;
-    currLen = traverseList(key, hashmap, 0);
-    if (currLen > maxLen) { maxLen = currLen; maxKey = key; }
-  }
   return maxLen;
 };
 
 console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 2]));
+// console.log(longestConsecutive([100, 0, 8, 4, 200, 3, 1, 2]));
+// console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 0, 0, 2]));
+console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 0, 2, 5]));
+//console.log(longestConsecutive([100]));
+console.log(longestConsecutive([0, 0]));
 
