94bb6a9dc570b115b278427651a0787d4e32f3a7
Ian Zhang <ianzhang1986@gmail.com>
2015-11-14 12:40:11 -0800

    [Feature] Understand the algorithm in longestConsecutive2.js

--- longestConsecutive2.js
+++ longestConsecutive2.js
@@ -8,7 +8,7 @@
 var longestConsecutive = function(nums) {
   'use strict';
   var hashmap = {};
-  var hashdup = {};
+  var hashdup = {}; // Remove duplicates
 
   if (nums.length === 0) return 0;
   if (nums.length === 1) return 1;
@@ -20,14 +20,13 @@ var longestConsecutive = function(nums) {
     if (hashdup[nums[iter]]) continue;
 
     if (hashmap[nums[iter]] === undefined) { hashmap[nums[iter]] = 1; hashdup[nums[iter]] = true; }
-    if (hashmap[nums[iter]+1] !== undefined) { hashmap[nums[iter]] += hashmap[nums[iter]+1]; console.log("num " + nums[iter] + " " + hashmap[nums[iter]]);}
+    if (hashmap[nums[iter]+1] !== undefined) { hashmap[nums[iter]] += hashmap[nums[iter]+1]; }
 
     if (hashmap[nums[iter]] > maxLen) { maxLen = hashmap[nums[iter]]; }
     // Keeps iterating till updating all previous ones ()
     prevKey = nums[iter] - 1; 
     while(hashmap[prevKey]) {
       hashmap[prevKey] += hashmap[nums[iter]];
-      console.log("num " + nums[iter] + " " + hashmap[nums[iter]]);
       if (hashmap[prevKey] > maxLen) { maxLen = hashmap[prevKey]; }
       prevKey -= 1;
     }
@@ -36,10 +35,11 @@ var longestConsecutive = function(nums) {
   return maxLen;
 };
 
-console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 2]));
+//console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 2]));
 // console.log(longestConsecutive([100, 0, 8, 4, 200, 3, 1, 2]));
 // console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 0, 0, 2]));
-console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 0, 2, 5]));
+// console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 0, 2, 5]));
+console.log(longestConsecutive([100, 8, 4, 200, 3, 1, 0, 2, 5, -1]));
 //console.log(longestConsecutive([100]));
-console.log(longestConsecutive([0, 0]));
+//console.log(longestConsecutive([0, 0]));
 
