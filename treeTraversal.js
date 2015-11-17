085fa652e0908e5fbd0bbf354f77b2a24f42c0fc
Ian Zhang <ianzhang1986@gmail.com>
2015-11-17 02:29:11 -0800

    [Feature] Fake pointer solution to deserialization

--- treeTraversal.js
+++ treeTraversal.js
@@ -6,8 +6,6 @@ var Tree = function(val) {
   this.left = this.right = null;
 };
 
-var treeNodes = [];
-
 // Construct tree from pre-order sequence
 
 /**
@@ -16,10 +14,44 @@ var treeNodes = [];
  */
 var constructor = function(arr) {
   if (!arr.length) return new Tree();
-
-  recurse(0, root);
+  this.arrayList = arr;
+  this.root = new Tree(this.arrayList[0]);
+  this.iter = 0;
+  this.deserialize(this.root, 1);
 };
 
 constructor.prototype.insert = function() {
   
 };
+
+constructor.prototype.deserialize = function(node, lr) {
+  this.iter+=1;
+  if (this.arrayList[this.iter] === -1 || this.arrayList[this.iter] === undefined) {
+    return;
+  }
+  
+  var currNode;
+  if (lr) { 
+    node.left = new Tree(this.arrayList[this.iter]); 
+    currNode = node.left;
+  } else {
+    node.right = new Tree(this.arrayList[this.iter]);
+    currNode = node.right;
+  }
+  this.deserialize(currNode, this.iter, 1);
+  this.deserialize(currNode, this.iter, 0);
+};
+
+constructor.prototype.serialize = function(root) {
+};
+
+constructor.prototype.peek = function() {
+//  console.log(this.root);
+  console.log(this.root.left.left);
+};
+
+// Test -->
+// var treeNodeList = [20, 8, 10, 5, -1, -1, -1, -1, -1];
+var treeNodeList = [20, 8, 10, 5, 6, -1, -1, -1, -1, -1];
+var tree = new constructor(treeNodeList);
+tree.peek();
