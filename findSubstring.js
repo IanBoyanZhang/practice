ebfb7f5a00bae63625040382004ad2881992a87d
Ian Zhang <ianzhang1986@gmail.com>
2015-11-17 16:01:31 -0800

    [Refactor] naive implementation findSubstring pass basic test case while time limit exceeded

--- findSubstring.js
+++ findSubstring.js
@@ -2,7 +2,8 @@
  * @param {string} s 
  * @param {string []} words 
  * @return {number []} 
- *
+ * Words are all in same length
+ * All word in word list has to present and only once
  */
 
 var findSubstring = function(s, words) {
@@ -10,15 +11,8 @@ var findSubstring = function(s, words) {
   if (s === "" || words.length === 0) return wBucket;
   var wLen = words[0].length, wordsLen = words.length, sLen = s.length;
   
-  var getWord = function(sIdx, arr) {
-    // arr -> s
-    var word = "";
-    for (var i = sIdx; i < wLen; i++) {
-      if(arr[i] === undefined) return "";
-      word+=arr[i];
-    }
-    return word;
-  };
+  // find effective candidates length
+  var effLen = wLen*wordsLen;
 
   var next = function(sIdx, wLen) {
     return sIdx += wLen;
@@ -27,32 +21,44 @@ var findSubstring = function(s, words) {
   var setupHash = function() {
     var hash = {};
     for (var iter = 0; iter < wordsLen; iter++) {
-      hash[words[iter]] = hash[words[iter]] || 1;
+      hash[words[iter]] = hash[words[iter]] || 0;
+      hash[words[iter]]++;
     }
     return hash;
   };
   
   var validator = function(str) {
     var hash = setupHash();
-    var hashLen = wordsLen;
-    var currWord = "";
+//    console.log(hash);
+//    console.log("In validator: " + str);
+    var currWord;
     for (var i = 0, len = str.length; i < len; ) {
-      currWord = getWord(i, str);
+      currWord = "";
+      for (var iter = 0; iter < wLen; iter++) {
+        currWord += str[i + iter];
+      }
+//      console.log("currWord-> ", currWord, "i: ", i, "str ", str);
       if (!hash[currWord]) return false;
       hash[currWord]-=1;
-      hashLen-=1;
       i = next(i, wLen);
     }
-    if (hashLen) return false;
     return true;
   };
 
   var substr = "";
-  for (var i = 0; i < sLen;) {
-    substr = s.slice(i);
+  for (var i = 0; i < sLen - effLen + 1;) {
+    substr = s.slice(i, i+effLen);
     if (validator(substr)) wBucket.push(i);
     i = next(i, wLen);
   }
 
   return wBucket;
 };
+
+var s = "barfoothefoobarman";
+var words = ["foo", "bar"];
+console.log(findSubstring(s, words));
+
+s = "wordgoodgoodgoodbestword";
+words = ["word","good","best","good"];
+console.log(findSubstring(s, words));
