783b08f53450b18122bc24d5ddc7c53343facf83
Ian Zhang <ianzhang1986@gmail.com>
2016-08-14 16:25:27 -0700

    [Update] 357 step2 of optimization: reuse partial prod

--- 357CountNumbers.js
+++ 357CountNumbers.js
@@ -6,6 +6,7 @@
  * question: definition of unique numbers
  * Combination problem
  * Optimization hash previous result
+ * Optimization reuse partial prod
  * super large number?
  */
 
@@ -18,7 +19,7 @@ var countNumbersWithUniqueDigits = function(n) {
     var res = 1;
     if (hash[m]) { return hash[m]; }
     for (var iter = 0; iter < m; iter += 1) {
-      res *= UNIT - iter;
+      res *= unit - iter;
     }
     hash[m] = res;
     return res;
@@ -33,7 +34,37 @@ var countNumbersWithUniqueDigits = function(n) {
   return count * UNIT + 10;
 };
 
+var countNumbersWithUniqueDigits = function(n) {
+  if (n === 0) { return 1; }
+  if (n === 1) { return 10; }
+  var UNIT = 9;
+  var hash = [9];
+
+  for(var iter = 1; iter < n && iter < 9; iter += 1) {
+    hash[iter] = hash[iter - 1] * (UNIT - iter);
+  }
+  var prod = function(m) {
+    if (m < 9) { return hash[m-1]; }
+    return hash[8-1];
+  };
+
+  var count = 0;
+  for (var i = 1; i < n && i < 10; i += 1) {
+    count += prod(i);
+  }
+
+  // + n(1)
+  return count * UNIT + 10;
+};
+
+
+
 console.log(countNumbersWithUniqueDigits(2));
 console.log(countNumbersWithUniqueDigits(3));
 console.log(countNumbersWithUniqueDigits(5));
 console.log(countNumbersWithUniqueDigits(11));
+console.log(countNumbersWithUniqueDigits(12));
+console.log(countNumbersWithUniqueDigits(13));
+console.log(countNumbersWithUniqueDigits(8));
+console.log(countNumbersWithUniqueDigits(9));
+console.log(countNumbersWithUniqueDigits(10));
