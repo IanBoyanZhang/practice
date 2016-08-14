2481153bdbae5dbdb1447159abecb75b2cc3de4f
Ian Zhang <ianzhang1986@gmail.com>
2016-08-14 15:37:18 -0700

    [Update] 357 first iter of optimization

--- 357CountNumbers.js
+++ 357CountNumbers.js
@@ -5,10 +5,7 @@
  * count all numbers with unique digits x, where 0 <= x < 10^n
  * question: definition of unique numbers
  * Combination problem
- * 211
- * 311
- * 411
- * 1211
+ * Optimization hash previous result
  * super large number?
  */
 
@@ -16,11 +13,14 @@ var countNumbersWithUniqueDigits = function(n) {
   if (n === 0) { return 1; }
   if (n === 1) { return 10; }
   var UNIT = 9;
+  var hash = [];
   var prod = function(m) {
     var res = 1;
+    if (hash[m]) { return hash[m]; }
     for (var iter = 0; iter < m; iter += 1) {
       res *= UNIT - iter;
     }
+    hash[m] = res;
     return res;
   };
 
