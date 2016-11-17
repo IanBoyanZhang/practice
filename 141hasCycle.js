bd53abd8c5879bdd5169a170170f873d72414098
Ian Zhang <ianzhang1986@gmail.com>
2016-11-17 08:56:06 -0800

    [Feature] 141hascycle

---
+++ 141hasCycle.js
@@ -0,0 +1,15 @@
+var hasCycle = function(head) {
+  var slow = head;
+  var fast = head;
+
+  while(fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
--- 19removeNthFromEnd.js
+++ 19removeNthFromEnd.js
@@ -7,6 +7,7 @@ function ListNode(val) {
  * Given linked list 1->2->3->4->5, and n = 2
  * from the end
  * After removing the second node from the end, the linked list becomes 1->2->3->5
+ * Convertion between array and linkedlist
  * @param [ListNode] head
  * @param {number} n
  * @return {ListNode}
