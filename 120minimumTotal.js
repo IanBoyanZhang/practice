8f90ac61d0e86507169658a22983a4340ffaa101
Ian Zhang <ianzhang1986@gmail.com>
2016-11-26 07:00:56 -0800

    [Feature] Remove curly brackets

--- 120minimumTotal.js
+++ 120minimumTotal.js
@@ -12,17 +12,15 @@ var minimumTotal = function(triangle) {
   var s = [];
   for (k = 0; k < m; k+=1) {
     c[0] = Infinity;
-    for (i = 0; i <= k; i+=1) {
+    for (i = 0; i <= k; i+=1) 
       c[i + 1] = k ? Math.min(t[k][i] + s[i], t[k][i] + s[i + 1]) : t[k][i];
-    }
     c[k + 2] = Infinity;
     s = c;
     c = [];
   }
   var min = s[1];
-  for (i = 1; i < m + 1; i+=1) {
+  for (i = 1; i < m + 1; i+=1)
     min = Math.min(s[i], min);
-  }
   return min;
 };
 var a =[[2], [3,4], [6,5,7], [4,1,8,3]];
