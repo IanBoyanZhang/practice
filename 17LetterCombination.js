9b65ebc15e0598bee9de0c94ca830c08e2676b0b
Ian Zhang <ianzhang1986@gmail.com>
2016-11-29 12:59:51 -0800

    [Feature] Using pseudo instead of substring for 17letterrecommandation

--- 17LetterCombination.js
+++ 17LetterCombination.js
@@ -17,4 +17,22 @@ var letterCombinations = function(digits) {
   return a;
 };
 
+// Faster than substring?
+var letterCombinations = function(digits) {
+  var a = [];
+  var t = ['', '*', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+  if (!digits) return a;
+  var L = digits.length;
+  var rec = function(d, b, id) {
+    if (id === L) return a.push(b);
+    var tb = t[Number(d[id])];
+    for (var i = 0; i < tb.length; i+=1)
+      rec(d, b + tb[i], id+1);
+  };
+  rec(digits, '', 0);
+  return a;
+};
+var digits = '123';
+var rtn = letterCombinations(digits);
+console.log(rtn);
 // How to optimize?
