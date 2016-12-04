eb5105e6669380e4379defa6df1ffc617e7c259e
Ian Zhang <ianzhang1986@gmail.com>
2016-12-04 01:31:48 -0800

    [Feature] 3lengthOfLongestSubstring

---
+++ 3lengthOfLongestSubstring.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ *
+ */
+var lengthOfLongestSubstring = function(s) {
+  var hash = {};
+  var start = 0;
+  var max = 0;
+  for(var i = 0, len = s.length; i < len; i+=1) {
+    var item = s[i];
+    if(!hash[item]) {
+      hash[item] = true;
+    } else {
+      // item in substring
+      // for second ptr start to catch up
+      for(;;){
+        console.log(start);
+        if (s[start] === item) {
+          start += 1;
+          break;
+        }
+        hash[s[start]] = false;
+        start += 1;
+      }
+    }
+    max = Math.max(max, i - start + 1);
+  }
+  return max;
+};
+
+//var str = "abcabcbb";
+//var str = "bbbbb";
+var str = "pwwkew";
+var str = "aab";
+var str = "bbbbb";
+var str = "abca";
+var rtn = lengthOfLongestSubstring(str);
+console.log(rtn);
