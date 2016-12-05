537b057a4a1e8a1a76960c571026b5c88ebe70f4
Ian Zhang <ianzhang1986@gmail.com>
2016-12-04 21:12:40 -0800

    [Feature] Remove duplicated solution

--- 5longestPalindrome.js
+++ 5longestPalindrome.js
@@ -14,7 +14,7 @@ var longestPalindrome = function(s) {
     }
     return R - L - 1;
   };
-  
+
   var start = 0, end = 0;
   for (var i = 0; i < s.length; i+=1) {
     var len1 = expandAroundCenter(s, i, 1);
@@ -33,31 +33,6 @@ var str = "aba";
 var str = "cbbd";
 var rtn = longestPalindrome(str);
 console.log(rtn);
-
-var longestPalindrome = function(s) {
-  var expandAroundCenter = function(s, L, R) {
-    while(L >= 0 && R < s.length && s.charAt(L) === s.charAt(R)) {
-      L--;
-      R++;
-    }
-    return R - L - 1;
-  };
-
-  var start = 0, end = 0;
-  for(var i = 0; i < s.length; i+=1) {
-    var len1 = expandAroundCenter(s, i, i);
-    var len2 = expandAroundCenter(s, i, i+1);
-    var len = Math.max(len1, len2);
-    if (len > end - start) {
-      start = i - Math.floor((len - 1)/2);
-      end = i + Math.floor(len/2);
-    }
-  }
-  console.log("HERE");
-  console.log(start);
-  console.log(end);
-  return s.substring(start, end + 1);
-};
 var str = "babad";
 var rtn = longestPalindrome(str);
 console.log(rtn);
