0cc2c9310789796d4443b36cb7e6f19dda6a76cf
Ian Zhang <ianzhang1986@gmail.com>
2016-12-10 13:53:24 -0800

    [Feature] insertionsort

--- Algorithm/sort/README.md
+++ Algorithm/sort/README.md
@@ -2,3 +2,26 @@
 
 ## Reading
 [Heapsort, Quicksort, and Entropy](http://users.aims.ac.za/~mackay/sorting/sorting.html)
+
+
+## Categories
+
+From wikipedia
+
+Exchange sorts: Quicksort
+Selection sorts: Heapsort
+Insertion sorts: Shellsort
+Merge sorts: Mergesort
+Distribution sorts: Bucket sort, Radix sort, counting sort
+Concurrent sorts:
+Hybrid sorts: Block merge sort, Timsort
+Other: Topological sorting ...
+
+
+Shell sort is not stable
+
+Divide and conquer: Mergesort, Quicksort
+
+
+## Real world implementation
+when element is less than 10, insertion sort is used
---
+++ Algorithm/sort/insertionsort.js
@@ -0,0 +1,26 @@
+// https://github.com/nzakas/computer-science-in-javascript/blob/master/algorithms/sorting/insertion-sort/insertion-sort.js
+var insertionsort = function(items) {
+  var len = items.length;
+  var value, i, j;
+
+  for (i=0; i<len; i+=1) {
+    // store the current value because it may shift later
+    value = items[i];
+    console.log(value);
+    /**
+     * whenever the value is in the sorted section is greater than the value
+     * in the unsorted section, shift all items in the sorted section over
+     * by one, This creates space in which to insert the value
+     */
+    for (j=i-1; j > -1 && items[j] > value; j--) {
+      items[j+1] = items[j];
+    }
+
+    items[j+1] = value;
+  }
+  
+  return items;
+};
+
+var arr = [6, 3, 5, 4, 2, 7, 1];
+console.log(insertionsort(arr));
