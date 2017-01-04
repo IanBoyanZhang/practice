5331fdc756437fda1dcb89f71b3139eea8e241a6
Ian Zhang <ianzhang1986@gmail.com>
2017-01-04 10:28:38 -0800

    [Feature] 138copyRandomList

---
+++ 138copyRandomList.js
@@ -0,0 +1,36 @@
+function RandomListNode(label) {
+  this.label = label;
+  this.next = this.random = null;
+}
+
+/**
+ * @param {RandomListNode} head
+ * @return {RandomListNode}
+ *
+ */
+var copyRandomList = function(head) {
+  if (!head) return null;
+
+  var node = head;
+  var hash = new Map();
+  var newArr = [];
+
+  while(node) {
+   hash.set(node, newArr.length);
+   newArr.push(new RandomListNode(node.label));
+   node = node.next;
+  }
+
+  node = head;
+  var index;
+  for(var i = 0, len = newArr.length; i < len; i+=1) {
+    if(i < len - 1)
+      newArr[i].next = newArr[i+1];
+    index = hash.get(node.random);
+    if (index !== undefined)
+      newArr[i].random = newArr[index];
+    node = node.next;
+  }
+
+  return newArr[0];
+};
