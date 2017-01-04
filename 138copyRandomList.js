0746126a206c337808c81321cf92d83a05e69276
Ian Zhang <ianzhang1986@gmail.com>
2017-01-04 11:11:27 -0800

    [Feature] 138 ES6 practice

--- 138copyRandomList.js
+++ 138copyRandomList.js
@@ -34,3 +34,29 @@ var copyRandomList = function(head) {
 
   return newArr[0];
 };
+
+var copyRandomList = function(head) {
+  if(!head) return null;
+
+  let node = head;
+  let newArr = [];
+  let hash = new Map();
+
+  while(node) {
+    hash.set(node, newArr.length);
+    newArr.push(new RandomListNode(node.label));
+    node = node.next;
+  }
+
+  node = head;
+  for (let i = 0, len = newArr.length; i < len; i+=1) {
+    if(i < len - 1) {
+      newArr[i].next = newArr[i + 1];
+    }
+    let index = hash.get(node.random);
+    if (index !== undefined)
+      newArr[i].random = newArr[index];
+    node = node.next;
+  }
+  return newArr[0];
+};
