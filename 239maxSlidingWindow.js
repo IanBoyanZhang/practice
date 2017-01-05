9c53cd12b70990ee230126b5ea87f397bf550b87
Ian Zhang <ianzhang1986@gmail.com>
2017-01-04 19:32:32 -0800

    [Feature] 239maxSlidingWindows

---
+++ 239maxSlidingWindow.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+// https://segmentfault.com/a/1190000003903509
+// https://github.com/chihungyu1116/leetcode-javascript/blob/master/239%20Sliding%20Window%20Maximum.js
+var maxSlidingWindow = function(nums, k) {
+  var result = [];
+  var linkedListWithTwoEndsOps = [];
+  var len = nums.length;
+  var i;
+
+  if (k > len || k === 0) {
+    return result;
+  }
+
+  for (i = 0; i < len; i+=1) {
+    // Remove anything that is less than the current value
+    // So linkedListWithTwoEndsOps maintains values greater than the current value
+    while (linkedListWithTwoEndsOps.length && nums[linkedListWithTwoEndsOps[linkedListWithTwoEndsOps.length - 1]] < nums[i]) {
+      linkedListWithTwoEndsOps.pop();
+    }
+    // In case that all elements in the linkedListWithTwoEndsOps are all greater than the current one (descending order)
+    // Shift out the 
+    if (linkedListWithTwoEndsOps[0] < i - k + 1) {
+      linkedListWithTwoEndsOps.shift();
+    }
+    linkedListWithTwoEndsOps.push(i);
+
+    // For each sliding window movement, we record the highest value in that sliding window
+    // i >= k - 1 to ensure that we don't prematurely record values before we get to the full range of the first sliding window
+    if (i >= k - 1) {
+      result.push(nums[linkedListWithTwoEndsOps[0]]);
+    }
+  }
+
+  return result;
+};
