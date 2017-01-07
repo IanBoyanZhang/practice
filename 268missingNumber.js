a4bf6f1ee6f62603c72ead78bf516fd94233bc04
Ian Zhang <ianzhang1986@gmail.com>
2017-01-07 00:42:26 -0800

    [Feature] 268 missing number using xor

--- 268missingNumber.js
+++ 268missingNumber.js
@@ -10,3 +10,15 @@ var missingNumber = function(nums) {
   }
   return sum + len;
 };
+
+// Slower
+// https://github.com/chihungyu1116/leetcode-javascript/blob/master/268%20Missing%20Number.js
+var MissingNumber = function(nums) {
+  var res = 0;
+
+  // index and num will cancel out by using xor
+  for (var i = 1; i < nums.length; i+=1) {
+    res = res ^ i ^ nums[i - 1];
+  }
+  return res;
+};
