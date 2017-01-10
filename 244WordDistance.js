191519dcbef5897d883c419e5f91d66b8c15d1c1
Ian Zhang <ianzhang1986@gmail.com>
2017-01-09 22:01:47 -0800

    [Feature] Add baseline solution to 244WordDistanceII

--- 244WordDistance.js
+++ 244WordDistance.js
@@ -5,7 +5,13 @@
  */
 // Time Limit Exceed (TLE)_
 var WordDistance = function(words) {
-  this.words = words;
+//  this.words = words;
+  this.positions = {};
+  var i, len = words.length;
+  for(i = 0; i < len; i+=1) {
+    this.positions[words[i]] = this.positions[words[i]] || [];
+    this.positions[words[i]].push(i);
+  }
 };
 
 /**
@@ -15,24 +21,24 @@ var WordDistance = function(words) {
  *
  */
 WordDistance.prototype.shortest = function(word1, word2) {
-  var len = this.words.length;
-  var hash = {};
-  var i, j;
-  for (i = 0; i < len; i+=1) {
-    hash[this.words[i]] = hash[this.words[i]] || [];
-    hash[this.words[i]].push(i);
-  }
+  var i = 0, j = 0;
+  var a = this.positions[word1];
+  var b = this.positions[word2];
 
-  var a = hash[word1];
-  var b = hash[word2];
-
-  if(!a.length || !b.length) {
+/*  if(!a.length || !b.length) {
     return null;
-  }
-  var min = Number.MAX_SAFE_INTEGER;
-  for(i = 0; i < a.length; i+=1) {
-    for(j = 0; j < b.length; j+=1) {
-      min = Math.min(min, Math.abs(a[i] - b[i]));
+  }*/
+//  var min = Number.MAX_SAFE_INTEGER;
+  var min = Infinity;
+  while(i < pos1.length && j < pos2.length) {
+    var i1 = a[i];
+    var i2 = b[j];
+
+    min = Math.min(min, Math.abs(i1 - i2));
+    if (i1 < i2) {
+      i+=1;
+    } else {
+      j+=1;
     }
   }
   return min;
