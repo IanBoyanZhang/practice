5888f158ef26546e7cf55afc2cde4b7c0572c7d5
Ian Zhang <ianzhang1986@gmail.com>
2017-01-24 19:48:07 -0800

    [Feature] Two pointer solution to 75 sort color

--- Code/75sortColors.js
+++ Code/75sortColors.js
@@ -7,5 +7,28 @@
 // in the order red, white, blue
 // 0. 1. 2
 var sortColors = function(nums) {
+  var len = nums.length;
+  var head = 0, tail = len - 1, i = 0;
 
+  function swap(arr, a, b) {
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+  while(i <= tail) {
+    if(nums[i] === 0) {
+      swap(nums, head, i);
+      head += 1;
+      i+=1;
+      continue;
+    }
+    if (nums[i] === 2) {
+      swap(nums, tail, i);
+      tail -=1;
+      // Thinking about why
+      // i+=1;
+      continue;
+    }
+    i += 1;
+  }
 };
