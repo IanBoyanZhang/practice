55f4f339c4b6bb6bfd8f0bebf3f105197efd0b06
Ian Zhang <ianzhang1986@gmail.com>
2017-06-21 17:48:14 -0700

    [Feature] 23 recursive solutions

--- C_CPP/linkedList/23Merge_k_sorted_lists.cpp
+++ C_CPP/linkedList/23Merge_k_sorted_lists.cpp
@@ -1,6 +1,6 @@
 typedef struct ListNode {
-  int data;
-  struct ListNode* Next;
+  double val;
+  struct ListNode* next;
 } ListNode;
 
 void swap(ListNode* l1, ListNode* l2) {
@@ -10,6 +10,12 @@ void swap(ListNode* l1, ListNode* l2) {
   l2 = temp;
 }
 class Solution {
-  ListNode* mergeTwoLists() {
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr)  {return l1;}
+    if (l2 == nullptr)  {return l2;}
+
+    if (l1->val> l2->val) { swap(l1, l2); }
+    l1->next = mergeTwoLists(l1->next, l2);
+    return l1;
   }
 };
