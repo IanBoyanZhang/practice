407723b6ed8a8144e77d895f8d219e020f5b1bb4
Ian Zhang <ianzhang1986@gmail.com>
2017-06-23 10:42:39 -0700

    [Feature] Update binary solution

--- C_CPP/linkedList/21Merge_2_sorted_lists.cpp
+++ C_CPP/linkedList/21Merge_2_sorted_lists.cpp
@@ -19,7 +19,7 @@ class Solution {
     return l1;
   }*/
   // iterative
-  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+  /*ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     ListNode head(-1);
     ListNode* p = &head;
     for (; l1!=nullptr || l2!=nullptr; p = p->next) {
@@ -35,5 +35,23 @@ class Solution {
       }
     }
     return head.next;
+  }*/
+
+  // A bit faster
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode *head = new ListNode(-1);
+    ListNode *tail = head;
+    while(l1 && l2) {
+      if (l1->val <= l2->val) {
+        tail->next = l1;
+        l1 = l1->next;
+      } else {
+        tail->next = l2;
+        l2 = l2->next;
+      }
+      tail = tail->next;
+    }
+    tail->next = l1 ? l1 : l2;
+    return head->next;
   }
 };
