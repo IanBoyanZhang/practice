94a28a74bbcb40a705e947746fb66fd0785095d0
Ian Zhang <ianzhang1986@gmail.com>
2017-07-02 09:28:14 -0700

    [Feature] 80 remove duplicates

---
+++ C_CPP/array/80removeDuplicates.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  int removeDuplicates(vector<int>& nums) {
+    if (nums.size() <= 2) return nums.size();
+
+    // index new array spot
+    // index also serves as occurence counter
+    int index = 2;
+    for (int i = 2; i < nums.size(); i +=1) {
+      if(nums[i] != nums[i - 2]) 
+        nums[index++] = nums[i];
+    }
+    return index;
+  }
+};
