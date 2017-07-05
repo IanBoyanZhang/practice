0e03af0b9bc87025e1d169c344ff4d5bc2127f81
Ian Zhang <ianzhang1986@gmail.com>
2017-07-04 17:56:19 -0700

    [Feature] inorder binary tree traversal recursively

---
+++ C_CPP/tree/94inorderTraversal.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+// Let's do it in recursive
+class Solution {
+public:
+  vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> stack;
+    dfs(root, stack);
+    return stack;
+  }
+
+  void dfs(TreeNode* r, vector<int>& res) {
+    if(!r) {return;}
+    dfs(r->left, res);
+    res.push_back(r->val);
+    dfs(r->right, res);
+  }
+};
