6985e33955ec115cbe3559c3eeb205c8fe2af83c
IanBoyanZhang <SillyKnight1986@gmail.com>
2018-01-16 04:20:22 -0800

    Add Chinese solution explanation

--- C_CPP/stack/85maximalRectangle.cpp
+++ C_CPP/stack/85maximalRectangle.cpp
@@ -1,5 +1,7 @@
 1. Ascending stack solution
 
+[Maximal rectangle solution explanation in Chn](https://siddontang.gitbooks.io/leetcode-solution/content/array/maximal_rectangle.html)
+
 [Maximal rectangle from Kamyu104](https://github.com/kamyu104/LeetCode/blob/master/C%2B%2B/maximal-rectangle.cpp)
 
 TODO: understand DP solution
