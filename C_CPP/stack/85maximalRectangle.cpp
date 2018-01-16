af088b8568a52b6376d7bdba694c9506e378844a
IanBoyanZhang <SillyKnight1986@gmail.com>
2018-01-16 04:22:25 -0800

    Update todos

--- C_CPP/stack/85maximalRectangle.cpp
+++ C_CPP/stack/85maximalRectangle.cpp
@@ -5,3 +5,7 @@
 [Maximal rectangle from Kamyu104](https://github.com/kamyu104/LeetCode/blob/master/C%2B%2B/maximal-rectangle.cpp)
 
 TODO: understand DP solution
+
+TODO: read more about algorithm design and thinking
+
+[Dr. Dobbs the maximal rectangle problem](http://www.drdobbs.com/database/the-maximal-rectangle-problem/184410529)
