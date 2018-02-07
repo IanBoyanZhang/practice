ed23c60161ed799c79369ff477881b252c6568f0
IanBoyanZhang <SillyKnight1986@gmail.com>
2018-02-07 04:21:34 -0800

    [Feature] Updated solution for 3

--- Code/3lengthOfLongestSubstring.js
+++ Code/3lengthOfLongestSubstring.js
@@ -38,3 +38,25 @@ var str = "bbbbb";
 var str = "abca";
 var rtn = lengthOfLongestSubstring(str);
 console.log(rtn);
+
+// https://leetcode.com/articles/longest-substring-without-repeating-characters/
+// Sliding window optimized
+// Define a mapping of the characters to its index
+// Then we can skip the characters immediately when we found a repeated character
+// No need to skip over little by little. 
+// let i be j + 1 directly
+// Skip over entirely : i = Math.max
+
+var lengthOfLongestSubstring = function(s) {
+	var j = 0, i = 0;
+	var hash = {};
+	var ans = 0;
+	for (; j < s.length; j+=1) {
+		if (hash[s[j]] !== undefined) {
+			i = Math.max(hash[s[j]], i);
+		}
+		ans = Math.max(ans, j - i + 1);
+		hash[s[j]] = j + 1;
+	}
+	return ans;
+};
