716ebc6dedbd4b798c92867a1d96fd2359450f09
IanBoyanZhang <SillyKnight1986@gmail.com>
2018-02-18 19:23:16 -0800

    [Feature] 227 Basic calculator

---
+++ Code/227calculate.js
@@ -0,0 +1,64 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// Non recursive fast solution
+// Two passes solution
+var calculate = function(s) {
+	var lex = [];
+	var tokens = [];
+
+	var symbolTable = {
+		'*': 1,
+		'/': 1,
+		'+': 1,
+		'-': 1,
+	};
+
+	var j = 1;
+	var digits = s[0];
+
+	for(;j < s.length; j+=1) {
+		if(s[j] === ' ') continue;
+		if(symbolTable[s[j]]) {
+			lex.push(+digits);
+			digits = '';
+			lex.push(s[j]);
+			continue;
+		}
+		digits += s[j];
+	}
+
+	lex.push(+digits);
+
+	var i = 1;
+	tokens.push(+lex[0]);
+	for(;i<lex.length;i+=1) {
+		if (lex[i] === '*') {
+			tokens.push(+tokens.pop() * + lex[i+=1]);
+			continue;
+		}
+
+		if(lex[i] === '/') {
+			var last = +tokens.pop();
+			if(last >= 0)
+				tokens.push(Math.floor(last/+lex[i+=1]));
+			else
+				tokens.push(-Math.floor(Math.abs(last/lex[i+=1])));
+			continue;
+		}
+
+		if(lex[i] === '+') {
+			tokens.push(+lex[i+1]);
+			continue;
+		}
+
+		if(lex[i] === '-') {
+			tokens.push(-lex[i+1]);
+			continue;
+		}
+	}
+
+	return tokens.reduce(function(a, b) { return a + b; }, 0);
+}
--- comp_geometry/README.md
+++ comp_geometry/README.md
@@ -1,3 +1,11 @@
 # Basic operations of Computational Geometry
 
+At the moment, not tested!
+
 1. template code for checking if two segments are intersected
+
+2. Convex hull
+
+3. Point in polygon
+
+4. Area of polygon
