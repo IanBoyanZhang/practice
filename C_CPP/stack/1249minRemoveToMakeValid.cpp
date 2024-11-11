ebc4cca59bc2438c2ab42c6b3e4e8bc7ba591246
Ian Zhang <ianzhang1986@gmail.com>
2024-11-11 09:06:10 -0800

    Create 1249minRemoveToMakeValid.cpp

---
+++ C_CPP/stack/1249minRemoveToMakeValid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        std::string result;
+        int unmatchedOpen = 0, openCount = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                openCount++;
+                unmatchedOpen++;
+                result += c;
+            } else if (c == ')') {
+                if (unmatchedOpen > 0) {
+                    unmatchedOpen--;
+                    result += c;
+                }
+            } else {
+                result += c;
+            }
+        }
+
+        // Second pass within the same result string to remove extra '('
+        int matchedOpenCount = openCount - unmatchedOpen; // Final valid '(' count
+        std::string finalResult;
+        finalResult.reserve(s.length());
+        // reserve
+        for (char c: result) {
+            if (c == '(') {
+                if (matchedOpenCount > 0) {
+                    finalResult += c;
+                    matchedOpenCount--;
+                }
+            } else {
+                finalResult += c;
+            }
+        }
+
+        return finalResult;
+    }
+};
